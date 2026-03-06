3f57cb404296fcc56ab75b3ba2bc73fceeec22ab
Juan Jose Vasquez-Mesa <juanjosevasquezmesa@gmail.com>
2026-03-06 12:15:40 -0500

    Design_Changes
    March 6th 12:15PM

--- js/main.js
+++ js/main.js
@@ -33,3 +33,27 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer) {
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
+const navBtns = document.querySelectorAll(".nav-btn");
+
+navBtns.forEach(btn => {
+
+  btn.addEventListener("mousemove", e => {
+
+    const rect = btn.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const moveX = (x - rect.width / 2) * 0.2;
+    const moveY = (y - rect.height / 2) * 0.2;
+
+    btn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+  });
+
+  btn.addEventListener("mouseleave", () => {
+    btn.style.transform = "translate(0,0)";
+  });
+
+});
